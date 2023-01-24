594f762c62c3ab7af4578d5f347361e0dc3a8a8f
Markus Doan-Le <markus.doanle@gmail.com>
2023-01-24 23:18:31 +0000

    Added Maximum Subarray LC practice

---
+++ max-subarray.js
@@ -0,0 +1,9 @@
+function maxSubArray(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+    for(let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(currentSum + nums[i], nums[i]);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+    return maxSum;
+};
--- two-sum.js
+++ two-sum.js
@@ -4,14 +4,14 @@
 
 let twoSum = (nums, target) => {
   let map = new Map();
-  for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let num1 = nums[i];
     let num2 = target - num1;
-    if(map.has(num2)) {
+    if (map.has(num2)) {
       return [map.get(num2), i]
     }
     map.set(num1, i);
   }
 }
 
-twoSum([2,7,11,15], 9)
+twoSum([2, 7, 11, 15], 9)
