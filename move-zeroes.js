90bea88e33f9a1268e3d31b6b3c813584eeadfa4
Markus Doan-Le <markus.doanle@gmail.com>
2023-01-24 23:39:27 +0000

    Added moveZeroes problems

---
+++ move-zeroes.js
@@ -0,0 +1,12 @@
+function moveZeroes(nums) {
+    let index = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            let temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+            index++;
+        }
+    }
+    return nums;
+}
