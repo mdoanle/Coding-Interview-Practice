d7c60bba8f47487657d33422547aa960da2ae2be
Markus Doan-Le <markus.doanle@gmail.com>
2023-02-03 01:29:14 +0000

    Solved fibonacci sequence using recursion

--- Algorithms/Recursion/fibonacci.js
+++ Algorithms/Recursion/fibonacci.js
@@ -6,20 +6,20 @@
 //For example: fibonacciRecursive(6) should return 8
 
 
-// function fibonacciIterative(n){
-//   let arr = [0, 1];
-//   for (let i = 2; i < n + 1; i++){
-//     arr.push(arr[i - 2] + arr[i -1]);
-//   }
-//  return arr[n];
-// }
-// fibonacciIterative(3);
+function fibonacciIterative(n){
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1]);
+  }
+ return arr[n];
+}
+fibonacciIterative(3);
 
 function fibonacciRecursive(n) {
-  if(n < 2){
+  if (n < 2){
     return n;
   }
-  return fibonacciRecursive(n-2) + fibonacciRecursive(n-1);
+  return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2)
 }
 
-console.log(fibonacciRecursive(3));
+fibonacciRecursive(6)
