f7f57db51e1033ca87147b37f2de11c9fe8611cc
Markus Doan-Le <markus.doanle@gmail.com>
2023-02-06 23:59:24 +0000

    added reverse string recursive

---
+++ Algorithms/Recursion/reverse-string-recursive.js
@@ -0,0 +1,10 @@
+//Implement a function that reverses a string using iteration...and then recursion!
+function reverseStringRecursive(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
+}
+
+reverseStringRecursive('yoyo master');
