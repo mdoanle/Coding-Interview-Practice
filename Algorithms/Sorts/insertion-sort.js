20fee0e4912e13b8db19bf5f469799f00e1f66d9
Markus Doan-Le <markus.doanle@gmail.com>
2023-02-07 18:49:16 +0000

    implemented insertion sort algo

--- Algorithms/Sorts/insertion-sort.js
+++ Algorithms/Sorts/insertion-sort.js
@@ -8,7 +8,7 @@ function insertionSort(array) {
     } else {
       for(let j = 1; j < length; j++){
         if(array[i] > array[j - 1] && array[i] < array[j]){
-          array.splice(j,0,array.splice(i,1)[0]);
+          array.unshift(array.splice(i,1)[0]);
         }
       }
     }
