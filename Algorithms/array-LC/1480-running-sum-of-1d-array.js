14bc334cbe4f276a5fbc837a5c18da44ae796ee8
Markus Doan-Le <markus.doanle@gmail.com>
2023-02-07 22:19:52 +0000

    completed LC 1480 running sum of 1d array

--- Algorithms/array-LC/1480-running-sum-of-1d-array.js
+++ Algorithms/array-LC/1480-running-sum-of-1d-array.js
@@ -1,18 +1,11 @@
-
-const nums = [1,2,3,4]
-//Output: [1,3,6,10]
-
 var runningSum = function(nums) {
    const ans = [];
     for(let i = 0; i < nums.length; i++){
       if(i === 0){
         ans.push(nums[i])  
       } else {
-        ans.push(ans[i-1] + nums[i])
+          ans.push(nums[i-1] + nums[i])
       }
-      console.log("answer var:", ans);
+    return ans;
     }
-  return ans;
 };
-
-runningSum(nums);
